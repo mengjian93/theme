500f31a97a9f8ed01ca59e34d9d1ccaf0a966e06
mengjian93 <mjgetjob@gmail.com>
2015-01-06 16:53:46 +0800

    饼图数据类

---
+++ src/com/chart/vo/PieData.java
@@ -0,0 +1,20 @@
+package com.chart.vo;
+
+
+public class PieData {
+	private String value;
+	private String name;
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
